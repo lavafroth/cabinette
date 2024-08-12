83f7ba1cfa64557b15dfc8dbbf76d74bf33e7843
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-08-12 17:10:48 +0530

    chore: clean up flake

--- flake.nix
+++ flake.nix
@@ -6,33 +6,18 @@
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
-          # config.cudaSupport = true;
         };
       in {
         devShells.${system}.default = (pkgs.buildFHSUserEnv {
-          name = "nvidia-fuck-you";
+          name = "cuda-mamba-devenv";
           targetPkgs = pkgs: (with pkgs; [
             linuxPackages.nvidia_x11
-            # cudatoolkit
-            # cudaPackages.cudnn
             libGLU libGL
             xorg.libXi xorg.libXmu freeglut
             xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib 
             ncurses5 stdenv.cc binutils
             ffmpeg
             fish
-            # (python311.withPackages(ps: with ps; [
-            #   pytorch-bin
-            #   sentence-transformers
-            #   scikit-learn
-            #   seqeval
-            #   pandas
-            #   numpy
-            #   transformers
-            #   jupyter
-            #   spacy
-            #   lt
-            # ]))
             pkg-config
             openssl.dev
             micromamba
@@ -43,7 +28,6 @@
               export CUDA_PATH="${pkgs.cudatoolkit}"
               export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
               export EXTRA_CCFLAGS="-I/usr/include"
-              echo FUCK NVIDIA FUCK NVIDIA
           '';
 
           runScript = "fish";
