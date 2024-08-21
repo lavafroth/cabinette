521b9eff0426e9dfa8fe386e12804e1b60993abb
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-08-21 08:57:26 +0530

    feat: interface between rust and javascript code

--- rust/src/main.js
+++ rust/src/main.js
@@ -3,16 +3,37 @@ const { invoke } = window.__TAURI__.tauri;
 let greetInputEl;
 let greetMsgEl;
 
-async function greet() {
+async function infer(text) {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+  chipletsEl.textContent = await invoke("infer", { contents: text });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
+  chipletsEl = document.querySelector("#chiplets");
+  // greetMsgEl = document.querySelector("#greet-msg");
+  // document.querySelector("#greet-form").addEventListener("submit", (e) => {
+  //   e.preventDefault();
+  //   greet();
+  // });
+
+  const plusIconEl = document.querySelector('#plus-icon');
+  const mainPageEl = document.querySelector('#main-page');
+  const editPageEl = document.querySelector('#edit-page');
+  plusIconEl.addEventListener('click', (e) => {
+    mainPageEl.classList.toggle('m-fadeOut');
+    mainPageEl.classList.toggle('m-fadeIn');
+    editPageEl.classList.toggle('m-fadeIn');
+    editPageEl.classList.toggle('m-fadeOut');
   });
+
+  let timer;
+  const waitTime = 500;
+  const recipeInstEl = document.querySelector('#recipe-instructions');
+  recipeInstEl.addEventListener('keyup', (e)=>{
+    const text = e.currentTarget.value;
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      infer(text);
+    }, waitTime);
+  })
 });
